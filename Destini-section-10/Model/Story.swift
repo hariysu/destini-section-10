05c383bf12d2bcbffe3942cdac840826c2f57654
Abdurahman AYDIN <hariysu@gmail.com>
2023-09-30 23:55:00 +0300

    Completes step 6

--- Destini-section-10/Model/Story.swift
+++ Destini-section-10/Model/Story.swift
@@ -10,7 +10,7 @@ import Foundation
 struct Story {
     var title: String
     var choice1: String
-    //var choice1Destination: Int
+    var choice1Destination: Int
     var choice2: String
-    //var choice2Destination: Int
+    var choice2Destination: Int
 }
